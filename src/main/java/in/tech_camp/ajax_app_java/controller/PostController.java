7ef3c2b019d7fd5a5f9dac8f70f1c5ee184b5e38
teramoto-gif <teramoto.kai@minebea-ss.com>
2026-06-16 16:42:36 +0900

    リクエストを処理する

--- src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
+++ src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
@@ -1,5 +1,6 @@
 package in.tech_camp.ajax_app_java.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,12 +32,14 @@ public class PostController {
   // }
 
   @PostMapping("/posts")
-  public String savePost(@ModelAttribute("postForm") PostForm form){
-    // System.out.println("メソッド呼び出し:" + form);
+  public ResponseEntity<PostEntity> savePost(@ModelAttribute("postForm") PostForm form){
+    // System.out.println("メソッド呼び出し:" + form); デバッグコード
     PostEntity post = new PostEntity();
     post.setContent(form.getContent());
     postRepository.insert(post);
-    return "redirect:/";
+    PostEntity resultPost = postRepository.findById(post.getId());
+    // System.out.println(resultPost); デバッグコード
+    return ResponseEntity.ok(resultPost);
   }
   
 }
